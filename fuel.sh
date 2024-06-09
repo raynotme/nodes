27bce41d968171928b751a3dddbe302a489ac3d9
raynotme <phunh.2510@gmail.com>
2024-06-09 16:33:27 +0700

    init fuel

---
+++ fuel.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1
+
+NODE="fuel"
+export DAEMON_HOME=$HOME/.fuel
+export DAEMON_NAME=fuel
+
+if [ ! $VALIDATOR ]; then
+    read -p "Enter validator name: " VALIDATOR
+    echo 'export VALIDATOR='\"${VALIDATOR}\" >> $HOME/.bash_profile
+fi
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+sleep 1
+cd $HOME
+sudo apt update
+sudo apt install make clang pkg-config lz4 libssl-dev build-essential git jq ncdu bsdmainutils htop -y < "/dev/null"
+
+echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
+cd $HOME
+VERSION=1.21.6
+wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+go version
+
+echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
+
+echo "fuel-core run
+--service-name=fuel-sepolia-testnet-node
+--keypair $FUEL_P2P_PRIVATE_KEY
+--relayer $ETHEREUM_RPC_ENDPOINT
+--ip=0.0.0.0 --port=4000 --peering-port=30333
+--db-path=~/.fuel-sepolia-testnet
+--snapshot ./chain-configuration/ignition/
+--utxo-validation --poa-instant false --enable-p2p
+--reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf
+--sync-header-batch-size 100
+--enable-relayer \
+--relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA
+--relayer-da-deploy-height=5827607
+--relayer-log-page-size=500
+--sync-block-stream-buffer-size 30
+" > /usr/local/bin/$DAEMON_NAME
+
+$DAEMON_NAME config keyring-backend test
+
+echo "[Unit]
+Description=$NODE Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/$DAEMON_NAME start
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target" > $HOME/$DAEMON_NAME.service
+sudo mv $HOME/$DAEMON_NAME.service /etc/systemd/system
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+#echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable $DAEMON_NAME
+sudo systemctl restart $DAEMON_NAME
+
+
+echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
+if [[ `service $DAEMON_NAME status | grep active` =~ "running" ]]; then
+  echo -e "Your $NODE node \e[32minstalled and works\e[39m!"
+  echo -e "You can check node status by the command \e[7mservice $DAEMON_NAME status\e[0m"
+  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+else
+  echo -e "Your $NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
+fi
