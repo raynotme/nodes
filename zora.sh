ec4b68e35292a3fd725c1c4be235a07df4549015
raynotme <phunh.2510@gmail.com>
2024-06-14 10:31:02 +0700

    Update zora.sh

--- zora.sh
+++ zora.sh
@@ -32,9 +32,10 @@ sudo apt install make clang pkg-config lz4 libssl-dev build-essential git jq ncd
 echo -e '\n\e[42mInstall Docker\e[0m\n' && sleep 1
 cd $HOME
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
-sudo install -m 0755 -d /etc/apt/keyringscurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 
@@ -44,7 +45,8 @@ git clone https://github.com/conduitxyz/node.git
 cd node
 ./download-config.py zora-mainnet-0
 export CONDUIT_NETWORK=zora-mainnet-0
-echo "OP_NODE_L1_ETH_RPC=$ETHEREUM_RPC_ENDPOINT" > $HOME/$DAEMON_NAME/node/.env
+echo "OP_NODE_L1_ETH_RPC=$ETHEREUM_RPC_ENDPOINT
+OP_NODE_L1_BEACON=https://beaconstate.info" > $HOME/$DAEMON_NAME/node/.env
 
 echo "docker compose up --build" > /usr/local/bin/$DAEMON_NAME
 
