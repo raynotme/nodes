ea6237a96246086023149586d5b22ce027ce42b5
raynotme <phunh.2510@gmail.com>
2024-06-14 10:39:12 +0700

    Update zora.sh

--- zora.sh
+++ zora.sh
@@ -35,11 +35,12 @@ sudo apt install curl build-essential git screen jq pkg-config libssl-dev libcla
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg -y
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 
 echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
+sudo mkdir -p $HOME/$DAEMON_NAME
 cd $HOME/$DAEMON_NAME
 git clone https://github.com/conduitxyz/node.git
 cd node
